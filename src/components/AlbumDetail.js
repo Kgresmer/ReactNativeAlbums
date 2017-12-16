b9332d9a4f260453ebf6f9a74905551b73c52f32
kgresmer <kgresmer@gmail.com>
2017-12-16 09:07:59 -0600

    Added lots fo style! Added album image added artist image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,60 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const {
+        thumbnailStyle,
+        headerTextStyle,
+        thumbnailContainerStyle,
+        headerAlbumTextStyle,
+        albumImageStyle
+    } = styles;
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle}
+                           source={{uri: thumbnail_image}}
+                    />
+                </View>
+                <View style={headerTextStyle}>
+                    <Text style={headerAlbumTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image style={albumImageStyle} source={{uri: image}}/>
             </CardSection>
         </Card>
     )
+};
+
+const styles = {
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    headerTextStyle: {
+        justifyContent: 'space-around',
+        flexDirection: 'column'
+    },
+    headerAlbumTextStyle: {
+        fontSize: 18
+    },
+    albumImageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
 };
 
 export default AlbumDetail;
